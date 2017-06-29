b719501016b4b199bda8e41ac50cf1188dbd08c3
Ronan Merien <ronan.merien@gmail.com>
2017-06-29 12:21:48 +0200

    initial commit

---
+++ src/pages/add-film/add-film.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-add-film',
+  templateUrl: 'add-film.html'
+})
+export class AddFilmPage {
+
+  constructor(public navCtrl: NavController) {
+  }
+  
+}
---
+++ src/pages/list-favorite/list-favorite.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-list-favorite',
+  templateUrl: 'list-favorite.html'
+})
+export class ListFavoritePage {
+
+  constructor(public navCtrl: NavController) {
+  }
+  
+}
